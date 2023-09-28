eaf5b393b49d98362eb2df9d77ecd8a3469d398e
AmjadAlshboul <144245691+AmjadAlshboul@users.noreply.github.com>
2023-09-28 18:31:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ function getAdvice(){
 	.then(response => response.json())
 	.then(json => {
 		const advice=json.slip.advice;
+		const idValue=json.slip.id;
 		const quote = document.querySelector('.card__quote');
+		const id = document.querySelector('.card__title__id');
 		console.log(advice);
 		
 		quote.innerHTML = `${advice}`;
+		id.innerHTML = `#${idValue}`
 	});
 }
